5213b94b7dcaec98d03ef4588e2d69b5f88aab90
bhupendra <bhupendrayadav2732@gmail.com>
2025-02-12 00:09:27 +0530

    Completing the customSecurityConfig

--- url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -3,8 +3,15 @@ package com.url.shortener.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.url.shortener.security.jwt.JwtAuthenticationFilter;
 import com.url.shortener.service.UserDetailsServiceImpl;
@@ -18,12 +25,73 @@ import com.url.shortener.service.UserDetailsServiceImpl;
 public class WebSecurityConfig {
 	
 	@Autowired
-	private UserDetailsServiceImpl userDetailsServiceImpl;
+	private UserDetailsServiceImpl userDetailsService;
 	
+	/*
+	 * @Bean is an annotation used to indicate that a method should return an object 
+	 * @Returns : object
+	 */
 	@Bean
 	public JwtAuthenticationFilter jwtAuthenticationFilter() {
 		return new JwtAuthenticationFilter();
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	/*
+	 * Configure DaoAuthenticationProvider:
+	 * 1.Sets up how authentication is handled by Spring security
+	 * 2.Bean for AuthenticationManager and PasswordEncoder
+	 */
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		//Instance of DaoAuthenticationProvider
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+		
+	}
+	
+	
+	
+	
+	/*
+	 * Custom security configuration
+	 * 
+	 * Configure JwtAuthenticationFilter in FilterChain : 
+	 * 1. Spring Security recognizes it as a filter chain
+	 * 2.By default, SpringSecurity does't automatically include your custom filter (JwtAuthenticationFilter) in the filterChain
+	 * unless we explicitly add it.
+	 * 
+	 * short url = http://domain.com/xyz --> google.com so http://domain.com/xyz should be accessible publicly so if its a short url
+	 * then permit all 
+	 * 
+	 * @Return : Object of type security filter chain
+	 */
+	
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/api/auth/**").permitAll()
+					.requestMatchers("/api/urls/**").authenticated()
+					.requestMatchers("/{shortUrl}").permitAll()
+					.anyRequest().authenticated()
+					);
+		
+		/*
+		 * We are here telling  springSecurity that before this (UsernamePasswordAuthenticationFilter) in the filterChain 
+		 * add this (jwtAuthenticationFilter) custom filter. 
+		 * 
+		 * Also setting authenticationProvider
+		 */
+		http.authenticationProvider(authenticationProvider());
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
 
 }
